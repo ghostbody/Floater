6e54ce20b577e17c67312e1cc65bc1e8ef165dcb
Ye Jiaqi <423093883@qq.com>
2015-12-20 19:13:33 +0800

    update client

--- src/client/chat.py
+++ src/client/chat.py
@@ -6,16 +6,16 @@ import time
 from config import *
 import threading
 
-def me(username):
-    server_name = "192.168.1.102"
+def send(username, server_name):
+    #server_name = "192.168.1.102"
     server_port = 8001
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     time.sleep(5)
     try:
         mySocket.connect((server_name, server_port))
     except socket.error, msg:
-        print '[FLOATER ERROR] Connect failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        return False
+        print '[FLOATER ERROR] Connect failed. Error Code : ' + str(msg[0]) + ' ' + msg[1]
+        return
 
     while True:
         message = raw_input()
@@ -26,8 +26,8 @@ def me(username):
             print "[FLOATER ERROR] ", e
             break
 
-def far(username):
-    server_name = "192.168.1.207"
+def receive(username, server_name):
+    #server_name = "192.168.1.207"
     server_port = 8001
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     time.sleep(5)
@@ -47,8 +47,15 @@ def far(username):
 
 def test():
     print  "[FLOATER CHATER 1.0] Online line"
-    t1 = threading.Thread(target=me, args=("Alice",))
-    t2 = threading.Thread(target=far, args=("Bob",))
+    # remote server
+    server_name_remote = "192.168.1.102"
+    server_name_local  = "192.168.1.207"
+    # username
+    username = "Alice"
+
+    t1 = threading.Thread(target=receive, args=(username, server_name_local))
+    t2 = threading.Thread(target=send, args=(username, server_name_remote))
+
     t1.start()
     t2.start()
 
