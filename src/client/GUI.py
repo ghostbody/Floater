622ead41f64a538b938dc1d3b77503bac7c7b3ef
Ye Jiaqi <423093883@qq.com>
2015-12-21 21:14:22 +0800

    update GUI

---
+++ src/client/GUI.py
@@ -0,0 +1,71 @@
+import sys
+from PyQt4 import QtCore, QtGui, QtWebKit
+
+import chat
+
+import threading
+import os
+
+html = open(os.path.join("front", "index.html")).read()
+
+class Floater(QtCore.QObject):
+    """Simple class with one slot and one read-only property."""
+
+    @QtCore.pyqtSlot(str)
+    def showMessage(self, msg):
+        """Open a message box and display the specified message."""
+        QtGui.QMessageBox.information(None, "hahhahah", msg)
+
+    def getUsername(self):
+        """Return the Python version."""
+        return "Bob"
+
+    @QtCore.pyqtSlot(str)
+    def send(self, message):
+        chat.send_queue.put("%s: %s" % ("Bob", messge))
+
+    @QtCore.pyqtSlot(str)
+    def receive(self):
+        message = chat.receive_queue.get()
+        q.task_done()
+        return message
+
+    """Python interpreter version property."""
+    username = QtCore.pyqtProperty(str, fget=getUsername)
+
+def main():
+
+    print  "[FLOATER CHATER 1.0] Online line"
+    # remote server
+    server_name_remote = "192.168.1.147"
+    server_name_local  = "192.168.1.207"
+    # username
+    username_local = "Alice"
+    username_remote = "Bob"
+
+    # start two thread
+    # thread t1 is a scoket that connect to the remote peer, and receive messgae from remote
+    t1 = threading.Thread(target=chat.receive, args=(username_remote, server_name_local))
+    # thread t2 is a socket that listen a port in local and send message to remote
+    t2 = threading.Thread(target=chat.send, args=(username_local, server_name_remote))
+
+    t1.start()
+    t2.start()
+
+    app = QtGui.QApplication(sys.argv)
+
+    myObj = Floater()
+
+    webView = QtWebKit.QWebView()
+    # Make myObj exposed as JavaScript object named 'pyObj'
+    webView.page().mainFrame().addToJavaScriptWindowObject("pyObj", myObj)
+    webView.setHtml(html)
+
+    window = QtGui.QMainWindow()
+    window.setCentralWidget(webView)
+    window.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
--- src/client/chat.py
+++ src/client/chat.py
@@ -1,34 +1,42 @@
 import socket
 import json
-import user
 import time
 
 from config import *
 import threading
+import Queue
+
+send_queue = Queue.Queue()
+receive_queue = Queue.Queue()
 
 def send(username, server_name):
     #server_name = "192.168.1.102"
-    server_port = 8001
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     time.sleep(5)
-    try:
-        mySocket.connect((server_name, server_port))
-    except socket.error, msg:
-        print '[FLOATER ERROR] Connect failed. Error Code : ' + str(msg[0]) + ' ' + msg[1]
-        return
+    while True:
+        try:
+            mySocket.connect((server_name, server_port))
+            break;
+        except socket.error, msg:
+            print '[FLOATER ERROR] Connect failed. Error Code : ' + str(msg[0]) + ' ' + msg[1]
+            time.sleep(1)
 
     while True:
-        message = raw_input()
-        print "[%s]: %s" % (username, message)
+        message = send_queue.get()
+        send_queue.task_done()
         try:
             mySocket.send(message)
         except Exception as e:
             print "[FLOATER ERROR] ", e
             break
+        # if user send close close message, close the connection
+        if message == "$$CLOSE$$":
+            print "[FLOATER CLOSE CONNECTION] connection closed by you"
+            break
+        print "[%s]: %s" % (username, message)
 
 def receive(username, server_name):
     #server_name = "192.168.1.207"
-    server_port = 8001
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     time.sleep(5)
     try:
@@ -42,19 +50,33 @@ def receive(username, server_name):
     print "[FLOATER ACCEPT CONNECTION] ", address
 
     while True:
-        message = connection.recv(1024)
-        print "[%s]: %s" % (username, message)
+        try:
+            message = connection.recv(1024)
+        except Exception as e:
+            print "[FLOATER ERROR] ", e
+            return
+
+        # if get close messge from remote, then close connection
+        if message == "$$CLOSE$$" or message == "":
+            print "[FLOATER CLOSE CONNECTION] connection closed by remote"
+            connection.close()
+            return
+        q.put("%s: %s" % (username, messge))
 
 def test():
     print  "[FLOATER CHATER 1.0] Online line"
     # remote server
-    server_name_remote = "192.168.1.102"
+    server_name_remote = "192.168.1.147"
     server_name_local  = "192.168.1.207"
     # username
-    username = "Alice"
+    username_local = "Alice"
+    username_remote = "Bob"
 
-    t1 = threading.Thread(target=receive, args=(username, server_name_local))
-    t2 = threading.Thread(target=send, args=(username, server_name_remote))
+    # start two thread
+    # thread t1 is a scoket that connect to the remote peer, and receive messgae from remote
+    t1 = threading.Thread(target=receive, args=(username_remote, server_name_local))
+    # thread t2 is a socket that listen a port in local and send message to remote
+    t2 = threading.Thread(target=send, args=(username_local, server_name_remote))
 
     t1.start()
     t2.start()
