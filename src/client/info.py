a03e76a3d195a1ee319e963e431489a3a1d64c7a
ghostbody <423093883@qq.com>
2016-01-10 20:02:16 +0800

    update project complete

--- src/client/info.py
+++ src/client/info.py
@@ -2,8 +2,8 @@ import time
 
 def info(message, messtype):
     if messtype == "info":
-        print "\033[32m[FLOATER v1.0 %s]\033[0m %s : %s" % (time.ctime(time.time()), "INFO", message)
+        print "[FLOATER v1.0 %s] %s : %s" % (time.ctime(time.time()), "INFO", message)
     elif messtype == "warning":
-        print "\033[32m[FLOATER v1.0 %s]\033[0m %s : %s" % (time.ctime(time.time()), "WARNING", message)
+        print "[FLOATER v1.0 %s] %s : %s" % (time.ctime(time.time()), "WARNING", message)
     elif messtype == "error":
-        print "\033[32m[FLOATER v1.0 %s]\033[0m %s : %s" % (time.ctime(time.time()), "ERROR", message)
+        print "[FLOATER v1.0 %s] %s : %s" % (time.ctime(time.time()), "ERROR", message)
